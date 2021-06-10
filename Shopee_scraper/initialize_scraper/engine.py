594d17e31fd75f9b7c59c5c7cc7b3bf58f506fc0
hanafication <rahardianardani@gmail.com>
2021-06-10 09:28:47 +0700

    Initial commit - Building the structure

---
+++ Shopee_scraper/initialize_scraper/engine.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+def engine(url):
+    ''''
+    Initializing webdriver to get url
+    '''
+# Setting
+    chrome_options = webdriver.ChromeOptions()
+#    chrome_options.add_argument('--headless')
+#    chrome_options.add_argument('--no-sandbox')
+#    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option('detach', True)
+
+    driver = webdriver.Chrome('C:/Users/Rahadian/Documents/Python Scripts/chromedriver_win32/chromedriver.exe', chrome_options=chrome_options)
+    driver.get(url=url)
+    return driver
---
+++ Shopee_scraper/main.py
@@ -0,0 +1,3 @@
+from initialize_scraper.engine import engine
+
+page = engine(url = 'https://shopee.co.id/')
